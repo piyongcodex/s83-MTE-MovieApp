264f4c3ef1bb25bfde965044623554a8ccc727a0
rio.sumandal0907 <rio.sumandal0907@gmail.com>
2024-07-13 21:27:24 +0800

    Ad imageurl

--- models/Movie.js
+++ models/Movie.js
@@ -25,6 +25,10 @@ const movieSchema = new mongoose.Schema({
     type: String,
     required: [true, "Genre is required"],
   },
+  imageUrl: {
+    type: String,
+    required: [true, "Image URL is required"],
+  },
   comments: {
     type: [
       {
